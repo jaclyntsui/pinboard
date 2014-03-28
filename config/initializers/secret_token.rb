5c4ca50513813f4a4ae2c8eab76946ea4f132cb8
Jaclyn Tsui <jaclyn.tsui@gmail.com>
2014-03-28 10:23:49 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinboard::Application.config.secret_key_base = 'a5cb80b126989479903c2067ed85b3c1b610400fa35c00463c5235fec3adaa9f5ab876c4fc41d08074d9cf929b740466d2f7e161f5d5bd716ac683b3e7be0f40'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pinboard::Application.config.session_store :cookie_store, key: '_pinboard_session'
