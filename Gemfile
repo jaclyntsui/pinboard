4146610562681b43a4de47fe0674bdb2b61aa3e3
Jaclyn Tsui <jaclyn.tsui@gmail.com>
2014-04-01 15:13:53 -0400

    Add Amazon S3 sdk for paperclip uploads to Heroku

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'jbuilder', '~> 1.2'
 gem 'bootstrap-sass'
 gem 'devise', '~>3.2.4'
 gem 'paperclip', github: 'thoughtbot/paperclip'
+gem 'aws-sdk', '~>1.20.0'
 
 group :development, :test do
 	# Use sqlite3 as the database for Active Record
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,14 @@ Pinboard::Application.configure do
   #required for Heroku
   #note to set this to your actual host
   config.action_mailer.default_url_options = { host: 'jt-pinboard.com' }
+
+  #Sets paperclip to upload images to amazon s3
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
