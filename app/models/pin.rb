6bc733197e112605f696b47fa61696ab1f373da8
Jaclyn Tsui <jaclyn.tsui@gmail.com>
2014-04-01 19:49:06 -0400

    Added validation for input fields

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,4 +4,8 @@ class Pin < ActiveRecord::Base
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 
 	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
+
+	validates :description, presence: true
+	validates :image, presence: true
+
 end
